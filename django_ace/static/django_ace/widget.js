c590465b1a107098e163d8025c85e350c9e8ba25
nandhra702 <sukhrajsinghnandhra@gmail.com>
2025-07-15 07:33:38 +0000

    copy-paste-prevention

--- django_ace/static/django_ace/widget.js
+++ django_ace/static/django_ace/widget.js
@@ -7,7 +7,6 @@
             Math.max(D.body.clientHeight, D.documentElement.clientHeight)
         );
     }
-
     function getDocWidth() {
         var D = document;
         return Math.max(
@@ -16,7 +15,6 @@
             Math.max(D.body.clientWidth, D.documentElement.clientWidth)
         );
     }
-
     function next(elem) {
         // Credit to John Resig for this function
         // taken from Pro JavaScript techniques
@@ -25,7 +23,6 @@
         } while (elem && elem.nodeType != 1);
         return elem;
     }
-
     function prev(elem) {
         // Credit to John Resig for this function
         // taken from Pro JavaScript techniques
@@ -34,7 +31,6 @@
         } while (elem && elem.nodeType != 1);
         return elem;
     }
-
     function redraw(element) {
         element = $(element);
         var n = document.createTextNode(' ');
@@ -44,11 +40,9 @@
         }).defer();
         return element;
     }
-
     function minimizeMaximize(widget, main_block, editor) {
         if (window.fullscreen == true) {
             main_block.className = 'django-ace-editor';
-
             widget.style.width = window.ace_widget.width + 'px';
             widget.style.height = window.ace_widget.height + 'px';
             window.fullscreen = false;
@@ -58,29 +52,32 @@
                 'width': widget.offsetWidth,
                 'height': widget.offsetHeight
             };
-
             main_block.className = 'django-ace-editor-fullscreen';
-
             widget.style.height = getDocHeight() + 'px';
             widget.style.width = getDocWidth() + 'px';
-
             window.scrollTo(0, 0);
             window.fullscreen = true;
         }
         editor.resize();
     }
-
     function apply_widget(widget) {
         var div = widget.firstChild,
             textarea = next(widget),
             editor = ace.edit(div),
             mode = widget.getAttribute('data-mode'),
             theme = widget.getAttribute('data-theme'),
-            default_light_theme = widget.getAttribute('data-default-light-theme'),
-            default_dark_theme = widget.getAttribute('data-default-dark-theme'),
             wordwrap = widget.getAttribute('data-wordwrap'),
             toolbar = prev(widget),
             main_block = toolbar.parentNode;
+            toolbar = (function findToolbar(elem) {
+                while (elem = elem.previousSibling) {
+                    if (elem.nodeType === 1 && elem.classList.contains('django-ace-toolbar')) {
+                        return elem;
+                    }
+                }
+                return null;
+            })(widget),
+            main_block = toolbar ? toolbar.parentNode : null;
 
         // Toolbar maximize/minimize button
         var min_max = toolbar.getElementsByClassName('django-ace-max_min');
@@ -88,48 +85,35 @@
             minimizeMaximize(widget, main_block, editor);
             return false;
         };
+        if (toolbar) {
+            var min_max = toolbar.getElementsByClassName('django-ace-max_min');
+            if (min_max.length > 0) {
+                min_max[0].onclick = function () {
+                    minimizeMaximize(widget, main_block, editor);
+                    return false;
+                };
+            }
+            // Add custom copy and paste buttons to the toolbar
+            addCustomCopyPasteButtons(toolbar, editor);
+        }
 
         editor.getSession().setValue(textarea.value);
 
         // the editor is initially absolute positioned
         textarea.style.display = "none";
-
         // options
         if (mode) {
             editor.getSession().setMode('ace/mode/' + mode);
         }
         if (theme) {
             editor.setTheme("ace/theme/" + theme);
-        } else {
-            if (window.matchMedia) {
-                const setEditorTheme = function (is_dark) {
-                    if (is_dark) {
-                        editor.setTheme("ace/theme/" + default_dark_theme);
-                    } else {
-                        editor.setTheme("ace/theme/" + default_light_theme);
-                    }
-                }
-
-                setEditorTheme(window.matchMedia('(prefers-color-scheme: dark)').matches);
-                try {
-                    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(ev) {
-                        setEditorTheme(ev.matches);
-                    })
-                } catch (err) {
-                    window.matchMedia('(prefers-color-scheme: dark)').addListener(function(ev) {
-                        setEditorTheme(ev.matches);
-                    })
-                }
-            }
         }
         if (wordwrap == "true") {
             editor.getSession().setUseWrapMode(true);
         }
-
         editor.getSession().on('change', function () {
             textarea.value = editor.getSession().getValue();
         });
-
         editor.commands.addCommands([
             {
                 name: 'Full screen',
@@ -181,25 +165,90 @@
                 }
             }
         ]);
-
         window[widget.id] = editor;
         $(widget).trigger('ace_load', [editor]);
     }
-
     function init() {
         var widgets = document.getElementsByClassName('django-ace-widget');
-
         for (var i = 0; i < widgets.length; i++) {
             var widget = widgets[i];
             widget.className = "django-ace-widget"; // remove `loading` class
-
             apply_widget(widget);
         }
     }
-
     if (window.addEventListener) { // W3C
         window.addEventListener('load', init);
     } else if (window.attachEvent) { // Microsoft
         window.attachEvent('onload', init);
     }
 })();
+
+document.addEventListener('copy', function(e) {
+    alert('Copy is disabled.');
+    e.preventDefault();
+});
+document.addEventListener('cut', function(e) {
+    alert('Cut is disabled.');
+    e.preventDefault();
+});
+document.addEventListener('paste', function(e) {
+    alert('Paste is disabled.');
+    e.preventDefault();
+});
+document.addEventListener('contextmenu', function(e) {
+    alert('Right-click is disabled.');
+    e.preventDefault();
+});
+document.addEventListener('keydown', function(e) {
+    if ((e.ctrlKey || e.metaKey) && ['c', 'v', 'x', 'C', 'V', 'X'].includes(e.key)) {
+        alert('Copy, cut, and paste shortcuts are disabled.');
+        e.preventDefault();
+    }
+});
+
+console.log('Custom [widget.js](http://_vscodecontentref_/5) loaded');
+
+function addCustomCopyPasteButtons(toolbar, editor) {
+    // Create Copy button
+    var copyBtn = document.createElement('button');
+    copyBtn.id = 'customCopyBtn';
+    copyBtn.textContent = 'Copy';
+    copyBtn.className = 'custom-copy-paste-btn';
+    copyBtn.onclick = function(event) {
+        event.preventDefault(); // prevent any default action like form submit or reload
+        window.tempClipboard = editor.getSelectedText();
+        sessionStorage.setItem('tempClipboard', window.tempClipboard);
+        console.log('Copied to temporary clipboard:', window.tempClipboard);
+        // alert('Copied to temporary clipboard!');
+    };
+
+    // Create Paste button
+    var pasteBtn = document.createElement('button');
+    pasteBtn.id = 'customPasteBtn';
+    pasteBtn.textContent = 'Paste';
+    pasteBtn.className = 'custom-copy-paste-btn';
+    pasteBtn.onclick = function(event) {
+        event.preventDefault();
+        var clipboardData = window.tempClipboard || sessionStorage.getItem('tempClipboard');
+        if (clipboardData) {
+            editor.insert(clipboardData);
+            window.tempClipboard = "";
+            sessionStorage.removeItem('tempClipboard');
+            console.log('Pasted from temporary clipboard');
+            // alert('Pasted from temporary clipboard!');
+        } else {
+            console.log('Clipboard is empty!');
+            // alert('Clipboard is empty!');
+        }
+    };
+
+    // Add buttons to toolbar
+    toolbar.appendChild(copyBtn);
+    toolbar.appendChild(pasteBtn);
+}
+
+// Removed local tempClipboard variable to avoid confusion
+
+// Optionally, block default copy/paste everywhere
+document.addEventListener('copy', function(e) { e.preventDefault(); });
+document.addEventListener('paste', function(e) { e.preventDefault(); });
