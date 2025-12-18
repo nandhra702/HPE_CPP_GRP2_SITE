0843d7490cd781dc92f03e75da2504fdcacccffd
LalithChowdary <v.lalithchowdary@gmail.com>
2025-12-18 11:19:56 +0530

    added mcq

--- hpe_admin/admin.py
+++ hpe_admin/admin.py
@@ -1,7 +1,9 @@
 from .sites import HPEAdminSite
-from judge.models import Problem
+from judge.models import Problem, MCQQuestion
 from judge.admin.problem import ProblemAdmin
+from judge.admin.mcq import MCQQuestionAdmin
 
 hpe_admin_site = HPEAdminSite(name='hpe_admin')
 
 hpe_admin_site.register(Problem, ProblemAdmin)
+hpe_admin_site.register(MCQQuestion, MCQQuestionAdmin)
